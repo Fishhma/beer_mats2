c5bca5984425452cd1e7f313ac435d095bcb0e39
Fishhma <146661839+Fishhma@users.noreply.github.com>
2025-12-11 13:00:20 +0100

    v2
    Add BASE_DIR and configure static and media file settings

--- beer_coasters/settings.py
+++ beer_coasters/settings.py
@@ -1,1 +1,12 @@
+from pathlib import Path
+
+# Сначала BASE_DIR
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Потом можно использовать BASE_DIR
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'coasters_app' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
