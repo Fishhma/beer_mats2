3e488c8086100d2a11143d836d883099b8f77a10
Fishhma <146661839+Fishhma@users.noreply.github.com>
2025-12-11 13:07:39 +0100

    v3

--- beer_coasters/settings.py
+++ beer_coasters/settings.py
@@ -10,3 +10,10 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # движок БД
+        'NAME': BASE_DIR / 'db.sqlite3',        # файл базы данных
+    }
+}
