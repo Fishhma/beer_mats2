03c1b754082809a69c1e999ca202a9dd3a670071
Fishhma <146661839+Fishhma@users.noreply.github.com>
2025-12-11 12:49:56 +0100

    v1

---
+++ coasters_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import User, Coaster
+
+admin.site.register(User)
+admin.site.register(Coaster)
---
+++ coasters_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class CoastersAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'coasters_app'
